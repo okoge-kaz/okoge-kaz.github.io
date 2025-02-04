1572498b0583bd878419a446528d512305d72e03
kazuki <kaz.tokyo.tech20@gmail.com>
2025-02-04 14:46:09 +0900

    feat: update portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -10,13 +10,30 @@ export interface Portfolio {
 export const portfolioData: Portfolio[] = [
   // Example entry
   {
-    title: "Causal Discovery Framework",
+    title: "vlm-recipes: VLM training Framework",
     description:
-      "A framework for discovering causal relationships in high-dimensional time series data using state-of-the-art machine learning techniques.",
-    technologies: ["Python", "PyTorch", "React"],
-    projectUrl: "https://project-demo.com",
+      "A framework for training vision-language models with PyTorch FSDP.",
+    technologies: ["Python", "PyTorch"],
     imageUrl:
-      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    codeUrl: "https://github.com/username/project",
+      "",
+    codeUrl: "https://github.com/turingmotors/vlm-recipes",
+  },
+  {
+    title: "moe-recipes: Mixture of Experts LLM training Framework",
+    description:
+      "A framework for training MoE with DeepSpeed.",
+    technologies: ["Python", "PyTorch"],
+    imageUrl:
+      "",
+    codeUrl: "https://github.com/okoge-kaz/moe-recipes",
+  },
+  {
+    title: "llm-recipes: LLM continual pre-training & post-training Framework",
+    description:
+      "A framework for training LLM with PyTorch FSDP for every huggingface dense LLMs.",
+    technologies: ["Python", "PyTorch"],
+    imageUrl:
+      "",
+    codeUrl: "https://github.com/okoge-kaz/llm-recipes",
   },
 ];
