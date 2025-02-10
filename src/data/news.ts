d3ff9ecc0243543198e2905a6a7ef1cf283c61af
kazuki <kaz.tokyo.tech20@gmail.com>
2025-02-11 02:19:04 +0900

    feat: add talk

--- src/data/news.ts
+++ src/data/news.ts
@@ -13,4 +13,18 @@ export const newsData: News[] = [
     description: "Our work on causal discovery in time series data has been accepted at ICML 2024.",
     link: "https://icml.cc/",
   }*/
+  {
+    date: "August 2024",
+    title: "Google Cloud Next '24 Tokyo Talk",
+    description:
+      "I gave a talk at Google Cloud Next '24 Tokyo on the topic of 'How to use Google Cluster Toolkit and real use-case'.",
+    link: "https://cloudonair.withgoogle.com/events/next-tokyo-24?talk=d2-inf-03"
+  },
+  {
+    date: "March 2024",
+    title: "NLP 2024 workshop talk",
+    description:
+      "I gave a talk at the NLP 2024 workshop on the topic of 'Distributed Training Technologies for Natural Language Processing'.",
+    link: "https://sites.google.com/view/llm-discussion-nlp2024-ws"
+  },
 ];
